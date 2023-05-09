9fe6200dc8c17669e2e542afe65906bbba180b30
Jingbo Su <sujingbo0217@gmail.com>
2023-05-09 13:41:54 +0800

    Completed lab1 [Added std lab1 and Fixed multi-line command analyzing]

--- Compiler-design-lab/lab1/input.c
+++ Compiler-design-lab/lab1/input.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*example*/
 static int a;
-const int b \
-    = 100;
-char * message = "Hello, world!";
-char ch = 'c';
+const int _b = 1\
+    00; // new line test
+char c = 'c';   // character test
+double d = 4.2; // float number test
+char * message = "Hello, world!";   // string test
 
 int main() {
   // test goto
-  101 : a = 2 * (1 + 3);
-  if (b > 10) {
+  L0: a = 2 * (1 + 3);
+  if (_b > 10) {
     a = 1;
-  } else if (b >= 5) {
+  } else if (_b >= 5) {
     a = 2;
   } else {
-    goto 101;
+    goto L0;
   }
 
-  /* Test command */
+  /* 
+   * Test 
+   * multi-line
+   * command 
+  **/
 
   // print answer
-  printf("%d\n", b);
-  printf("%s\n", message);
+  printf("%d\n", _b + a);
+  printf("%s", message);
+  puts("");
   return 0;
 }
